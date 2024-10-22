8ccbc67565037925b2e7c6bc946d13184a9bb3c0
onerandomusername <me@arielle.codes>
2024-10-22 07:34:35 +0000

    fix: allow loading src only build flags from env

--- env-extra.py
+++ env-extra.py
@@ -16,13 +16,26 @@ except ImportError:
 
 config = dotenv.dotenv_values(".env")
 
+
+LOCAL_VARS = {
+    "LOGGER_LEVEL",
+    "WIFI_SSID",
+    "WIFI_PASS",
+}
+
 global_flags = []
+local_flags = []
 for var, value in config.items():
     formatted_var = ""
     if value is not None:
         formatted_var = f'-D{var}="{value}"'
     else:
         formatted_var = f"-D{var}"
-    global_flags.append(formatted_var)
+    if var in LOCAL_VARS:
+        local_flags.append(formatted_var)
+    else:
+        global_flags.append(formatted_var)
 
 env.Append(BUILD_FLAGS=global_flags)  # type: ignore # noqa: F821
+# undef SUBNETMASK // conflicts with some other libraries
+env.Append(SRC_BUILD_FLAGS=local_flags)  # type: ignore # noqa: F821
